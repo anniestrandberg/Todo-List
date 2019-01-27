25be1ff8015974ccd78fc28d483f0eafebd11b8f
Annie Strandberg <anniestrandberg@Annies-MBP.lan>
2019-01-27 16:50:46 +0100

    First commit

---
+++ assets/js/todos.js
@@ -0,0 +1,22 @@
+$('ul').on('click', 'li', function(){
+    $(this).toggleClass("completed");
+});
+
+$('ul').on('click', 'span', function(e){
+    $(this).parent().fadeOut(1000, function(){
+        $(this.remove());
+    });
+    e.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13){
+        var todoText = $(this).val();
+        $(this).val('');
+        $('ul').append('<li><span><i class="far fa-trash-alt"></i></span>' + todoText + '</li>')
+        
+    }
+
+});
+
